10b2c0ea0286e09088814b4291dd942654a4c250
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2020-09-23 03:05:27 +0100

    homepage update and fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -237,7 +237,7 @@
     </div>
 
     <!--? Testimonial Start -->
-    <div class="testimonial-area testimonial-padding section-bg" data-background="/home/assets/img/gallery/section_bg04.jpg">
+    <div class="testimonial-area testimonial-padding section-bg" data-background="/home/assets/img/hero/testimonials.jpg">
         <div class="container">
             <div class="row justify-content-between">
                 <div class="col-xl-7 col-lg-7">
@@ -246,7 +246,7 @@
                         <span>Clients Testimonials</span>
                         <h2>What Our Investors Say!</h2>
                     </div>
-                    <div class="h1-testimonial-active mb-70">
+                    <div class="h1-testimonial-active mb-5">
                         <!-- Single Testimonial -->
                         <div class="single-testimonial ">
                             <!-- Testimonial Content -->
@@ -320,11 +320,11 @@
                 </div>
                 <!-- Form Start -->
                 <div class="col-xl-4 col-lg-5 col-md-8">
-                    <div class="testimonial-form text-center">
+                    {{-- <div class="testimonial-form text-center">
                         <h3>Always listening, always understanding.</h3>
                         <input type="text" placeholder="Incoterms">
                         <button name="submit" class="submit-btn">Request a Quote</button>
-                    </div>
+                    </div> --}}
                 </div>
                 <!-- Form End -->
             </div>
