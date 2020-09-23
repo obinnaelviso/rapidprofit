00e7f7c5a729c8f8b70d8867f50e7fa48c65709b
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2020-09-23 02:34:37 +0100

    Tawk Support

--- resources/views/layouts/tawk.blade.php
+++ resources/views/layouts/tawk.blade.php
@@ -1,13 +1,13 @@
-{{-- <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
-    s1.src='https://embed.tawk.to/5f58861b4704467e89ed5f6c/default';
+    s1.src='https://embed.tawk.to/5f6aa509f0e7167d0012dc8b/default';
     s1.charset='UTF-8';
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
     })();
-</script>
-    <!--End of Tawk.to Script--> --}}
+    </script>
+    <!--End of Tawk.to Script-->
