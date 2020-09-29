1786099cd1a62d5e388ab5217e8f1873b35c4e49
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2020-09-29 21:41:04 +0100

    favicon add home

--- resources/views/layouts/index/head.blade.php
+++ resources/views/layouts/index/head.blade.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="">
     <meta name="description" content="">
+	<link href="/favicon.png" rel="icon" type="image/png"/>
 
     <title>@yield('title')</title>
 
